909f8f7c24c4c88d2fea340206978fe3064254d7
Younes Lahouiti <loller763@gmail.com>
2023-06-01 10:40:15 +0200

    Initialization of a new project for the video canvas test

---
+++ src/utils/functions/helper-functions/console.functions.ts
@@ -0,0 +1,27 @@
+/**
+ * The console methods are exported as separate methods through destructuring
+ */
+export const {
+  log,
+  error,
+  table,
+  time,
+  timeEnd,
+  timeStamp,
+  timeLog,
+  assert,
+  clear,
+  count,
+  countReset,
+  group,
+  groupCollapsed,
+  groupEnd,
+  profile,
+  profileEnd,
+  trace,
+  warn,
+  debug,
+  info,
+  dir,
+  dirxml,
+} = console;
